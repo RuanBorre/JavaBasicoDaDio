839c1da8df8257ee3fcbf5d03ba053a48e4c211d
ruanborre <ruanborre245@gmail.com>
2024-07-02 11:40:20 -0300

    commit 022

--- anatomia-das-classes/src/MinhaClasse.java
+++ anatomia-das-classes/src/MinhaClasse.java
@@ -1,3 +1,22 @@
+/*Anotações 
+ * Todo arquivo ,java devera começar com letra maiuscula, por exemplo: MinhaClasse.java / Cliente.java /
+ * 
+ * Caso seja uma palavra composta a segunda palavra tambem devra seguir essa regra.
+ * 
+ * A classe devera ter o mesmo nome do arquivo, caso nao seja dara erro de escrita.
+ * 
+ * VARIAVEL deve ser escrita com letra minuscula, mas caso seja composta, a segunda palavra devra ser maiscula.
+ Para criar uma variavel que o valor nao seja alterado use (final), por exemplo:
+  final String BR= "Brasil";
+  String é usado como texto, para que BR receba Brasil.
+  Outro exemplo, uma variavel de ano:
+  esse tipo de variavel pode sofrer alterações, fazemos isso usando int, para especificar que podera ter alteração, fazemos assim: 
+ int ano = 2023;
+ ano = 2024;
+
+ * 
+*/
+
 public class MinhaClasse {
     public static void main (String []args){
         System.out.println("Olá, Mundo!");
