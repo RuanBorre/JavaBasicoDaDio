2ecedd48a9b907f5f81ed6713fdeeb4f44872138
ruanborre <ruanborre245@gmail.com>
2024-07-11 18:13:30 -0300

    commited

---
+++ anatomia-das-classes/src/Boletimestudantil.java
@@ -0,0 +1,13 @@
+public class Boletimestudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 10;
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if(mediaFinal==6)
+            System.out.println("RECUPERAÇÃO");
+        else 
+            System.out.println("APROVADO");
+    }
+}
+
+/*Mesmo que seja o mais basico que consiga fazer, se orgulhe de seus pequenos aprendizados! Estou orgulhoso de você. */
--- anatomia-das-classes/src/MinhaClasse.java
+++ anatomia-das-classes/src/MinhaClasse.java
@@ -19,6 +19,14 @@
 
 public class MinhaClasse {
     public static void main (String []args){
-        System.out.println("Olá, Mundo!");
+       String primeiroNome = "Ruan";
+       String segundoNome = "Borré";
+String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do Método: " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
